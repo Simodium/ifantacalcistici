ce447a92f02b7fbd225f6b088815bd2a358b1b37
Simodium <simodium@gmail.com>
2025-06-02 21:05:06 +0200

    Reinstallazione Tailwind + DaisyUI

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,16 @@
 export default function Home() {
   return (
-    <div>
-      <h1>Benvenuto!</h1>
-      <p>Questa è la pagina iniziale.</p>
-    </div>
+    <>
+      <div className="p-8">
+        <button className="btn btn-primary">Prova DaisyUI</button>
+        <div className="bg-pink-200 p-8 rounded-lg shadow-lg">
+          Prova Tailwind
+        </div>
+      </div>
+      <div>
+        <h1>Benvenuto!</h1>
+        <p>Questa è la pagina iniziale.</p>
+      </div>
+    </>
   );
 }
